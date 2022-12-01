0591d890f901855ae8f1e679d1c32904545e7432
Opilane TTHK <opilane@tthk.ee>
2022-12-01 11:10:19 +0200

    spaceshipIndex

--- TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
+++ TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Data;
+using TARge21Shop.Models.Spaceship;
 
 namespace TARge21Shop.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly TARge21ShopContext _context;
+
+        public SpaceshipsController
+            (
+                TARge21ShopContext context
+            )
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.SpaceShips
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    Passengers = x.Passengers,
+                    EnginePower = x.EnginePower
+                });
+            return View(result);
         }
     }
 }
