0da14ecb8ac9c9a7680d91c77a3176b9ee5443ea
Opilane TTHK <opilane@tthk.ee>
2022-12-01 09:24:08 +0200

    midagi

---
+++ TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TARge21Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
