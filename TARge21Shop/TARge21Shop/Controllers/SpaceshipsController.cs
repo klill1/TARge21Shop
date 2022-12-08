8e04e8b02893f632df2a5f778b89940212a25656
Opilane TTHK <opilane@tthk.ee>
2022-12-08 09:24:57 +0200

    AddServices

--- TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
+++ TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Core.Dto;
 using TARge21Shop.Data;
 using TARge21Shop.Models.Spaceship;
 
@@ -31,10 +32,40 @@ namespace TARge21Shop.Controllers
             return View(result);
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
+            SpaceshipEditViewModel spaceship = new SpaceshipEditViewModel();
 
-            return View("Edit");
+            return View("Edit", spaceship);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(SpaceshipEditViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                Type = vm.Type,
+                Crew = vm.Crew,
+                Passengers = vm.Passengers,
+                CargoWeight = vm.CargoWeight,
+                FullTripsCount = vm.FullTripsCount,
+                MaintenanceCount = vm.MaintenanceCount,
+                LastMaintenance = vm.LastMaintenance,
+                EnginePower = vm.EnginePower,
+                MaidenLaunch = vm.MaidenLaunch,
+                BuiltDate = vm.BuiltDate,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+
+            };
+
+
+
+            return View();
         }
     }
 }
