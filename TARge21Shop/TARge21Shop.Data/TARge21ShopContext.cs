d317ab025b06c644c52a68d39366b4ba32064e7d
klill1 <99496503+klill1@users.noreply.github.com>
2023-01-31 10:23:55 +0200

    .

--- TARge21Shop/TARge21Shop.Data/TARge21ShopContext.cs
+++ TARge21Shop/TARge21Shop.Data/TARge21ShopContext.cs
@@ -12,6 +12,6 @@ namespace TARge21Shop.Data
         public DbSet<Spaceship> SpaceShips { get; set; }
         public DbSet<FileToDatabase> FileToDatabases { get; set; }
         public DbSet<RealEstate> RealEstates { get; set; }
-        
+
     }
 }
